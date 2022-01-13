71e1ca87c0dc12f8dc6a14c74e2a484b23c1912d
renanrdias <renanrdias@gmail.com>
2022-01-12 23:22:53 -0300

    Changes to make it more general.

--- etl.py
+++ etl.py
@@ -5,17 +5,20 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dayofweek, monotonically_increasing_id
 
+
 config = configparser.ConfigParser()
-config.read('dl.cfg')
+config.read_file(open('dl.cfg'))
+
 
 os.environ['AWS_ACCESS_KEY_ID'] = config['AWS']['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS']['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_DEFAULT_REGION'] = config['AWS']['AWS_DEFAULT_REGION']
 
 
 def create_spark_session():
     spark = SparkSession \
         .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.10.1") \
         .getOrCreate()
     return spark
 
@@ -40,13 +43,17 @@ def process_song_data(spark, input_data, output_data):
     df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = df.select('song_id', 'title', 'artist_id', 'year', 'duration').drop_duplicates(['song_id'])
+    songs_table = df.select('song_id', 'title', 'artist_id', 'year', 'duration')\
+        .drop_duplicates(['song_id'])
 
     # write songs table to parquet files partitioned by year and artist
-    songs_table.write.partitionBy('year', 'artist_id').mode('overwrite').parquet(os.path.join(output_data, 'songs/songs.parquet'))
+    songs_table.write.partitionBy('year', 'artist_id')\
+        .mode('overwrite')\
+        .parquet(os.path.join(output_data, 'songs/songs.parquet'))
 
     # extract columns to create artists table
-    artists_table = df.select('artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude').drop_duplicates(['artist_id'])
+    artists_table = df.select('artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude')\
+        .drop_duplicates(['artist_id'])
 
     # write artists table to parquet files
     artists_table.write.mode('overwrite').parquet(os.path.join(output_data, 'artists/artists.parquet'))
@@ -103,14 +110,17 @@ def process_log_data(spark, input_data, output_data):
         .drop_duplicates(['start_time']).orderBy('start_time')
 
     # write time table to parquet files partitioned by year and month
-    time_table.write.partitionBy('year', 'month').mode('overwrite').parquet(os.path.join(output_data, 'times/time_data.parquet'))
+    time_table.write.partitionBy('year', 'month').mode('overwrite')\
+        .parquet(os.path.join(output_data, 'times/time_data.parquet'))
 
     # read in song data to use for songplays table
     song_data = os.path.join(input_data, 'song_data/*/*/*/*.json')
     song_df = spark.read.json(song_data)
 
     # extract columns from joined song and log datasets to create songplays table
-    songplays_table = df.join(song_df, [df.song == song_df.title, df.artist == song_df.artist_name, df.length == song_df.duration]).drop_duplicates()\
+    songplays_table = df\
+        .join(song_df, [df.song == song_df.title, df.artist == song_df.artist_name, df.length == song_df.duration])\
+        .drop_duplicates()\
         .withColumn('songplay_id', monotonically_increasing_id()) \
             .select(
                 col('songplay_id'),
@@ -126,17 +136,21 @@ def process_log_data(spark, input_data, output_data):
                 month('datetime').alias('month'))
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table.write.partitionBy('year', 'month').mode('overwrite').parquet(os.path.join(output_data, 'songplays/songplays.parquet'))
+    songplays_table.write.partitionBy('year', 'month').mode('overwrite')\
+        .parquet(os.path.join(output_data, 'songplays/songplays.parquet'))
 
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = "s3a://spark-bucket-rrmd/project-files/"
+
+    input_data = 's3a://udacity-dend/'
+    output_data = 's3a://spark-bucket-rrmd/project-files/'
 
     process_song_data(spark, input_data, output_data)
+
     process_log_data(spark, input_data, output_data)
 
 
 if __name__ == "__main__":
     main()
+
